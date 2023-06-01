ee32f3ac213464e59b9bf9d9521d4bdda4789540
Saksham <saksham.gairola28@gmail.com>
2023-06-01 12:34:05 +0530

    initial commit

---
+++ src/main/java/com/getUser/getUserAPI/GetUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.getUser.getUserAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetUserApiApplication.class, args);
+	}
+
+}
